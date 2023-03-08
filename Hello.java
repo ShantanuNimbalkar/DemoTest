644a45bbaa3cfe37fae6e96dcd266f2268e76609
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2023-03-08 16:41:20 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
   public static void main(String args[]){
     System.out.println("Hello World");
     System.out.println("Hey");
-    //System.out.println("bye");
+    System.out.println("bye");
   }
 }
